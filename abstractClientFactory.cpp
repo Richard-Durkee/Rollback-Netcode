fbbab0df8f20c3b6c00f60cdbb9a31a41ca4440b
Richard Durkee <richarddurkee1@gmail.com>
2024-08-12 19:20:57 -0400

    Client Factory Class

---
+++ abstractClientFactory.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Richard Durkee on 8/12/24.
+//
+
+#include "abstractClientFactory.h"
---
+++ abstractClientFactory.h
@@ -0,0 +1,22 @@
+//
+// Created by Richard Durkee on 8/12/24.
+//
+
+#ifndef ABSTRACTCLIENTFACTORY_H
+#define ABSTRACTCLIENTFACTORY_H
+
+
+
+class abstractClientFactory {
+public:
+    abstractClientFactory() = default;
+
+private:
+    ip_addr = 0.0.0.0
+
+};
+
+
+
+
+#endif //ABSTRACTCLIENTFACTORY_H
