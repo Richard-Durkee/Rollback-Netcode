9ccd1f646eb8389c85386f4ff3c94da70504b4c0
Richard Durkee <richarddurkee1@gmail.com>
2024-08-13 15:41:34 -0400

    Socket Polling Multiple Clients

---
+++ Packet.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Richard Durkee on 8/13/24.
+//
+
+#include "Packet.h"
---
+++ Packet.h
@@ -0,0 +1,17 @@
+//
+// Created by Richard Durkee on 8/13/24.
+//
+
+#ifndef PACKET_H
+#define PACKET_H
+
+
+
+class Packet {
+public:
+
+};
+
+
+
+#endif //PACKET_H
